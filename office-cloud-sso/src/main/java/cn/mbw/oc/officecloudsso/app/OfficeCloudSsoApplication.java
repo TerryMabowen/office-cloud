4860fc51f9dc35971151f94e2fa39234d5696fbb
apple_22 <mabw@bell.ai>
2020-03-17 20:56:23 +0800

    . #3

---
+++ office-cloud-sso/src/main/java/cn/mbw/oc/officecloudsso/app/OfficeCloudSsoApplication.java
@@ -0,0 +1,13 @@
+package cn.mbw.oc.officecloudsso.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeCloudSsoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfficeCloudSsoApplication.class, args);
+    }
+
+}
---
+++ office-cloud-sso/src/test/java/cn/mbw/oc/officecloudsso/OfficeCloudSsoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.mbw.oc.officecloudsso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficeCloudSsoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
