163111cc81fad4fbf39326aa418abcd55055c85e
apple_22 <mabw@bell.ai>
2020-03-19 20:29:47 +0800

    . #3

--- office-cloud-sso/src/main/java/cn/mbw/oc/officecloudsso/app/OfficeCloudSsoApplication.java
+++ office-cloud-sso/src/main/java/cn/mbw/oc/officecloudsso/app/OfficeCloudSsoApplication.java
@@ -2,8 +2,16 @@ package cn.mbw.oc.officecloudsso.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableCaching(proxyTargetClass = true)
+@EnableScheduling
+@EnableAsync
+@ComponentScan(basePackages = {"cn.mbw.oc"})
 public class OfficeCloudSsoApplication {
 
     public static void main(String[] args) {
