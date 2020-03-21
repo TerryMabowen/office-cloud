055d4cd9875cbab929173b1299191f925e1c337d
mabowen <2636965214@qq.com>
2020-03-21 19:39:49 +0800

    . #3

--- office-cloud-oc/src/main/java/cn/mbw/oc/web/app/OfficeCloudOcApplication.java
+++ office-cloud-oc/src/main/java/cn/mbw/oc/web/app/OfficeCloudOcApplication.java
@@ -2,8 +2,16 @@ package cn.mbw.oc.web.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableCaching(proxyTargetClass = true)
 @SpringBootApplication
+@ServletComponentScan
+@ComponentScan(basePackages = { "cn.mbw.oc" })
+@EnableScheduling
 public class OfficeCloudOcApplication {
 
     public static void main(String[] args) {
--- office-cloud-sso/src/main/java/cn/mbw/oc/sso/web/app/OfficeCloudSsoApplication.java
+++ office-cloud-sso/src/main/java/cn/mbw/oc/sso/web/app/OfficeCloudSsoApplication.java
@@ -2,16 +2,16 @@ package cn.mbw.oc.sso.web.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
 @EnableCaching(proxyTargetClass = true)
+@SpringBootApplication
+@ServletComponentScan
+@ComponentScan(basePackages = { "cn.mbw.oc" })
 @EnableScheduling
-@EnableAsync
-@ComponentScan(basePackages = {"cn.mbw.oc"})
 public class OfficeCloudSsoApplication {
 
     public static void main(String[] args) {
