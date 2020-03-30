f9f8017296caf576af5ff2b2095f96196ce66b88
apple_22 <mabw@bell.ai>
2020-03-30 21:06:39 +0800

    . #4

---
+++ office-cloud-sso/src/main/java/cn/mbw/oc/sso/biz/mapper/user/UserMapper.java
@@ -0,0 +1,15 @@
+package cn.mbw.oc.sso.biz.mapper.user;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+/**
+ * TODO
+ *
+ * @author Mabowen
+ * @date 2020-03-30 20:28
+ */
+@Repository
+@Mapper
+public interface UserMapper {
+}
